ceff7c960a00a9de1b6eb0d51412c8095022ee3b
Kiran Limbu <limbukiran6969@gmail.com>
2026-08-21 18:53:50 +0545

    push code into production

--- src/utils/ws.ts
+++ src/utils/ws.ts
@@ -2,7 +2,9 @@ import { io } from "socket.io-client";
 
 
 function connectWS(){
-    return io("http://localhost:4000");
+    const apiUrl = import.meta.env.VITE_API_URL
+    console.log(apiUrl)
+    return io(apiUrl);
 }
 
 export default connectWS;
