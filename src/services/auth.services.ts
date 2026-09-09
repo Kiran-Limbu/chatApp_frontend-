287d2cb0cb9256ebbc9327c4876bd53644fdb59f
Kiran Limbu <limbukiran6969@gmail.com>
2026-09-09 09:18:47 +0545

    #fix localstorage issue

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -5,12 +5,13 @@ import apiClientWraper from "../api/client.ts";
 export async function setUserCredentials() {
   try {
     const res = await apiClientWraper.get("/api/user/me");
+   
     localStorage.setItem(
       "info",
       JSON.stringify({
-        email: res.data.user.email,
-        userName: res.data.user.userName,
-        avatar: res.data.user.avatar,
+        email: res.data[0].email,
+        userName: res.data[0].userName,
+        avatar: res.data[0].avatar,
       }),
     );
   } catch (error) {
